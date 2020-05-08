4a45703e49a3580cfe76fee65379ad238bfa4753
Ed Brook <edbrook@gmail.com>
2020-05-08 17:26:30 +0100

    refactor: remove unused properties

--- src/main/java/uk/co/edbrook/oauth2/domain/OAuthServerProperties.java
+++ src/main/java/uk/co/edbrook/oauth2/domain/OAuthServerProperties.java
@@ -1,12 +1,9 @@
 package uk.co.edbrook.oauth2.domain;
 
 import lombok.Data;
-import org.hibernate.validator.constraints.Length;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.validation.annotation.Validated;
-
-import javax.validation.constraints.NotBlank;
 
 @Data
 @Validated
@@ -17,12 +14,4 @@ public class OAuthServerProperties {
     private long authCodeTimeout = 120;
 
     private long accessTokenTimeout = 3600;
-
-    @NotBlank
-    @Length(min = 32, max = 256)
-    private String authCodeSecret;
-
-    @NotBlank
-    @Length(min = 32, max = 256)
-    private String accessTokenSecret;
 }
